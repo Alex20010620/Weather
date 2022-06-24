b411135d8ea5e9c0d2c97cfae2fd90e1e8b52b15
Alex20010620 <58935675+Alex20010620@users.noreply.github.com>
2022-06-24 10:39:26 +0300

    second build

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.weather
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
